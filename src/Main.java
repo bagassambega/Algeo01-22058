3483f31e290d4a7d6e42b33d3c36417ec4cafcf9
bagassambega <bagassambega@gmail.com>
2023-09-26 13:51:47 +0700

    created matrix main constructor

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,103 @@
 import java.util.*;
 import matrix.*;
+import java.io.*;
+import java.lang.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Selamat datang di program kalkulator matriks!");
-        System.out.print("Pilih operasi yang ingin dilakukan:");
-        Scanner inputMain = new Scanner(System.in);
-        int choice = inputMain.nextInt();
-        System.out.println("1. Penyelesaian SPL");
-        System.out.println("2. Determinan Matriks");
-        System.out.println("3. Invers Matriks");
-        System.out.println("4. Interpolasi Polinomial");
-        System.out.println("5. Regresi Linear Berganda");
-        System.out.println("6. Interpolasi Bicubic Spline");
+        boolean mainLoop = true;
+        while (mainLoop) {
+            System.out.println("Selamat datang di program kalkulator matriks!");
+            Scanner inputMain = new Scanner(System.in);
+            System.out.println("1. Penyelesaian SPL");
+            System.out.println("2. Determinan Matriks");
+            System.out.println("3. Invers Matriks");
+            System.out.println("4. Interpolasi Polinomial");
+            System.out.println("5. Regresi Linear Berganda");
+            System.out.println("6. Interpolasi Bicubic Spline");
+            System.out.println("7. Keluar");
+            System.out.print("Pilih operasi yang ingin dilakukan: ");
+            int choice = inputMain.nextInt();
+
+            switch (choice) {
+                case 1:
+                    SPL();
+                    break;
+                case 2:
+                    System.out.println("\n==========Determinan Matriks==========");
+                    System.out.println("1. Metode eliminasi Gauss");
+                case 7:
+                    System.out.println("Terima kasih telah menggunakan program ini!");
+                    mainLoop = false;
+                    break;
+                default:
+                    System.out.println("Input tidak valid!");
+                    break;
+            }
+        }
+    }
+
+
+    public static void InputMatrix() {
+        Scanner inputMatrix = new Scanner(System.in);
+        System.out.print("Masukkan matrix melalui terminal (1) atau file (2)?: ");
+        int InputMatrixChoice = inputMatrix.nextInt();
+        while (true) {
+            if (InputMatrixChoice == 1 || InputMatrixChoice == 2) {
+                break;
+            }
+            else {
+                System.out.println("Input tidak valid!");
+                System.out.print("Masukkan matrix melalui terminal (1) atau file (2)?: ");
+                InputMatrixChoice = inputMatrix.nextInt();
+            }
+        }
+
+        switch (InputMatrixChoice) {
+            case 1:
+                Scanner inputRow = new Scanner(System.in);
+                Scanner inputCol = new Scanner(System.in);
+                System.out.print("\nMasukkan jumlah baris: ");
+                int row = inputRow.nextInt();
+                System.out.print("Masukkan jumlah kolom: ");
+                int col = inputCol.nextInt();
+                Matrix matCLI = new Matrix(row, col);
+                matCLI.readMatrixCLI(matCLI.row, matCLI.col);
+                matCLI.printMatrixCLI();
+                break;
+            case 2:
+                Scanner inputFilename = new Scanner(System.in);
+                System.out.print("\nMasukkan nama file: ");
+//                Matrix matFile = new Matrix();
+//                matFile.readMatrixFile(inputFilename.nextLine());
+//                matFile.printMatrixCLI();
+                break;
+            default:
+                System.out.println("Input tidak valid!");
+                break;
+        }
+    }
+
+
+    public static void SPL() {
+        System.out.println("\n==========SPL==========");
+        System.out.println("1. Metode eliminasi Gauss");
+        System.out.println("2. Metode eliminasi Gauss-Jordan");
+        System.out.println("3. Metode matriks balikan");
+        System.out.println("4. Metode Cramer");
+        System.out.print("Masukkan jenis penyelesaian SPL: ");
+        Scanner SPLchoice = new Scanner(System.in);
+        int SPLchoicenum = SPLchoice.nextInt();
+        while (true) {
+            if (SPLchoicenum > 4 || SPLchoicenum < 1) {
+                System.out.println("Input tidak valid!");
+                System.out.print("Masukkan jenis penyelesaian SPL: ");
+                SPLchoicenum = SPLchoice.nextInt();
+            }
+            else {
+                break;
+            }
+        }
+        InputMatrix();
     }
 }
--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -1,6 +1,10 @@
 package matrix;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+import java.io.*;
+import java.lang.*;
 
 public class Matrix {
     public int row;
@@ -11,7 +15,11 @@ public class Matrix {
     public Matrix(int row, int col) {
         this.row = row;
         this.col = col;
-        this.matrix = new double[row][col];
+        this.matrix = new double[this.row][this.col];
+    }
+
+    public double getElmt(int i, int j) {
+        return this.matrix[i][j];
     }
 
     public void readMatrixCLI(int row, int col) {
@@ -20,6 +28,25 @@ public class Matrix {
             for (int j = 0; j < col; j++) {
                 this.matrix[i][j] = scanElmt.nextDouble();
             }
+        }
+    }
+
+    public void readMatrixFile(String filename) {
+        try (FileReader fileInput = new FileReader(filename)) {
+            BufferedReader bufferedReader = new BufferedReader(fileInput);
+            StringBuilder line;
+            while (bufferedReader.readLine() != null) {
+                line = new StringBuilder(bufferedReader.readLine());
+                String[] lineArray = line.toString().split(" ");
+                for (int i = 0; i < lineArray.length; i++) {
+                    this.matrix[this.row][i] = Double.parseDouble(lineArray[i]);
+                }
+//                this.row++;
+
+            }
+        }
+        catch (IOException e) {
+            System.err.println(e);
         }
     }
 
