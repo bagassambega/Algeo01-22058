27fc84f6e5bec9b4a7c700946a08c1d1b797849b
HenryofSkalitz1202 <110165919+HenryofSkalitz1202@users.noreply.github.com>
2023-10-01 01:16:26 +0700

    Add files via upload

---
+++ src/Aplikasi/BicubicSpline.java
@@ -0,0 +1,190 @@
+import java.util.Scanner;
+
+public class BicubicSpline{
+    static double BicubicSplineEquation(double[][] mtrx, double[][] variables, double a, double b){
+        double value = 0;
+
+        for(int j = 0; j <= 3; j++){
+            for(int i = 0; i <= 3; i++){
+                value += variables[i][j] * Math.pow(a, i) * Math.pow(b, j);
+            }
+        }
+        return value;
+    }
+
+    static double[][] InverseOfMatrix(double[][] matrix, int order)
+    {
+        double temp;
+
+        for (int i = 0; i < order; i++) {
+            for (int j = 0; j < 2 * order; j++) {
+                if (j == (i + order))
+                    matrix[i][j] = 1;
+            }
+        }
+ 
+        for (int i = order - 1; i > 0; i--) {
+            if (matrix[i - 1][0] < matrix[i][0]) {
+                double[] tempArr = matrix[i];
+                matrix[i] = matrix[i - 1];
+                matrix[i - 1] = tempArr;
+            }
+        }
+
+        for (int i = 0; i < order; i++) {
+            for (int j = 0; j < order; j++) {
+
+                double pivot = matrix[i][i];
+                if(pivot == 0){
+                    for(int k = i+1; k < order; k++){
+                        if(matrix[k][i] == 0){continue;}
+                        else{
+                            double[] tempArr = matrix[i];
+                            matrix[i] = matrix[k];
+                            matrix[k] = tempArr;
+                            break;
+                        }
+                    }
+                }
+                pivot = matrix[i][i];
+
+                if (j != i) {
+                    temp = matrix[j][i] / pivot;
+                    for (int k = 0; k < 2 * order; k++) {
+                        matrix[j][k] -= matrix[i][k] * temp;
+                    }
+                }
+            }
+        }
+ 
+        for (int i = 0; i < order; i++) {
+            temp = matrix[i][i];
+            for (int j = 0; j < 2 * order; j++) {
+ 
+                matrix[i][j] = matrix[i][j] / temp;
+            }
+        }
+ 
+        double[][] inverse_matrix = new double[16][16];
+        int temp_col = 0;
+
+        for(int i = 0; i < 16; i++){
+            temp_col = 0;
+            for(int j = 16; j < 32; j++){
+                inverse_matrix[i][temp_col] = matrix[i][j];
+                
+                if(inverse_matrix[i][temp_col] == -0){
+                    inverse_matrix[i][temp_col] = 0.000000;
+                }
+
+                temp_col++;
+            }
+        }
+ 
+        return inverse_matrix;
+    }
+
+    static double[][] matrixX (double A[][]){
+        //baris 1-4
+        //(0, 0) -> (1, 0) -> (0, 1) -> (1,1)
+        int row = 0;
+        for(int y = 0; y <= 1; y++){
+            for(int x = 0; x <= 1; x++){
+                int col = 0;
+                
+                for(int j = 0; j <= 3; j++){
+                    for(int i = 0; i <= 3; i++){
+                        A[row][col] = Math.pow(x, i) * Math.pow(y, j);
+                        col++;
+                    }
+                }
+                row++;
+            }
+        }
+
+        //baris 5-8
+        for(int y = 0; y <= 1; y++){
+            for(int x = 0; x <= 1; x++){
+                int col = 0;
+                
+                for(int j = 0; j <= 3; j++){
+                    //pengganti i = 0
+                    A[row][col] = 0;
+                    col++;
+                    for(int i = 1; i <= 3; i++){
+                        A[row][col] = i* Math.pow(x, i-1) * Math.pow(y, j);
+                        col++;
+                    }
+                }
+                row++;
+            }
+        }
+
+        //baris 9-12
+        for(int y = 0; y <= 1; y++){
+            for(int x = 0; x <= 1; x++){
+                int col = 0;
+                
+                //pengganti j = 0
+                for(int k = 0; k <= 3; k++){
+                    A[row][col] = 0;
+                    col++;
+                }
+
+                for(int j = 1; j <= 3; j++){
+                    for(int i = 0; i <= 3; i++){
+                        A[row][col] = j* Math.pow(x, i) * Math.pow(y, j-1);
+                        col++;
+                    }
+                }
+                row++;
+            }
+        }
+
+        //baris 13-16
+        for(int y = 0; y <= 1; y++){
+            for(int x = 0; x <= 1; x++){
+                int col = 0;
+
+                for(int j = 0; j <= 3; j++){
+                    for(int i = 0; i <= 3; i++){
+                        A[row][col] = i*j* Math.pow(x, i-1) * Math.pow(y, j-1);
+                        Double current_elem = (Double)A[row][col];
+                        if (current_elem.isNaN()){
+                            A[row][col] = 0;
+                        }
+                        col++;
+                    }
+                }
+                row++;
+            }
+        }  
+        return A;
+    }
+
+    static double[][] inverseSPLBicubicSpline(double inversed[][], double constant[]){
+        double sum;
+
+        double[][] variable = new double[4][4];
+        int variable_row = 0;
+        int variable_col = 0;
+
+        int n = inversed.length;
+        for(int i = 0; i < n; i++){
+            sum = 0;
+            if(variable_row > 3){
+                    variable_col++;
+                    variable_row = 0;
+                }
+
+            for(int j = 0; j < n; j++){
+                sum += inversed[i][j] * constant[j];
+            }
+
+            variable[variable_row][variable_col] = sum;
+            variable_row++;
+        }
+
+        return variable;
+    }
+}
